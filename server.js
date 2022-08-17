f8b5dc048547d48727b3e517c879018c279cc658
Eduardo Sabino <68930625+edu-sabino@users.noreply.github.com>
2022-08-17 10:27:06 -0300

    Add files via upload
    Pasta com o nome Álvaro tem os arquivos criados por ele. Temos de torná-los nossas views com ejs.
    Falta muita coisa a ser feita ainda.

---
+++ server.js
@@ -0,0 +1,101 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+const users = []; // variável local para armazenar os usuários em memória
+const veiculos = []; // variável local para armazenar os carros em memória. Depois de criar o BD, excluir ela.
+
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false })); // para poder usar req.body
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+// usando o middleware checkAuthenticated antes de expor a página protegida por login
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+}); // página que a pessoa precisa estar logada para entrar
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+}); // página de entrada da aplicação, onde o usuário ou adm vão efetuar login
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/form", checkNotAuthenticated, (req, res) => {
+  res.render("form.ejs");
+}); // página onde o adm vai cadastrar um novo veículo
+
+app.post("/form", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(), // no BD isso é gerado automaticamente
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/");
+  } catch {
+    res.redirect("/form");
+  }
+  console.log(users);
+});
+
+app.delete("/logout", (req, res, next) => {
+  req.logOut(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  }); // fornecido pelo passport, encerra a sessão e nos direciona a outra página
+});
+
+// Middlewares
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.listen(3000);
